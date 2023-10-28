0503dcb6a3f4b73fdaefe49c88fcabf587f1b9d9
almondkiruthu <113090717+almondkiruthu@users.noreply.github.com>
2023-10-28 16:05:13 +0300

    intial commit

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,10 +1,8 @@
-import axios from "axios";
+import axios from 'axios';
 
 export default axios.create({
-  baseURL: "https://api.rawg.io/api",
+  baseURL: 'https://api.rawg.io/api',
   params: {
-    key: "c7b18323a47d40c394ea5b019646b1f5",
+    key: 'f5b56fb4fcdc43df8c293b0c1d550edf',
   },
 });
-
-// f5b56fb4fcdc43df8c293b0c1d550edf

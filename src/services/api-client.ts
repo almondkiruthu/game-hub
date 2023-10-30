6b1399dd7f53e6c75c39c2552ce7723e7a49cd9a
almondkiruthu <113090717+almondkiruthu@users.noreply.github.com>
2023-10-30 18:36:38 +0300

    Clean up project to start over using shadcn

--- src/services/api-client.ts
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-export default axios.create({
-  baseURL: 'https://api.rawg.io/api',
-  params: {
-    key: 'f5b56fb4fcdc43df8c293b0c1d550edf',
-  },
-});
-// TODO Understand the layout

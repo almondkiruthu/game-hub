26087a0c38b7925988062ae72bc0b535c8689f98
almondkiruthu <113090717+almondkiruthu@users.noreply.github.com>
2023-11-03 19:03:45 +0300

    Fetch Games

--- services/api-client.ts
+++ services/api-client.ts
@@ -1,8 +1,19 @@
 import axios from 'axios';
 
-axios.create({
+const apiClient = axios.create({
   baseURL: 'https://api.rawg.io/api',
   params: {
     key: 'f5b56fb4fcdc43df8c293b0c1d550edf',
   },
 });
+
+export default apiClient;
+
+// import axios from "axios";
+
+// export default axios.create({
+//   baseURL: "https://api.rawg.io/api",
+//   params: {
+//     key: "c7b18323a47d40c394ea5b019646b1f5",
+//   },
+// });
--- src/components/game-grid.tsx
+++ src/components/game-grid.tsx
@@ -1,5 +1,40 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+interface Game {
+  id: number;
+  name: string;
+}
+interface FetchGamesResponse {
+  count: number;
+  results: Game[];
+}
+
 const GameGrid = () => {
-  return <div>Games!</div>;
+  const [games, setGames] = useState<Game[]>([]);
+  const [error, setError] = useState('');
+
+  useEffect(() => {
+    axios
+      .get<FetchGamesResponse>('https://api.rawg.io/api/xgames', {
+        params: {
+          key: 'f5b56fb4fcdc43df8c293b0c1d550edf',
+        },
+      })
+      .then((res) => {
+        setGames(res.data.results);
+      })
+      .catch((err: Error) => setError(err.message));
+  }, []);
+  return (
+    <>
+      {error && <p>{error}</p>}
+      <ul>
+        {games.map((game) => (
+          <li key={game.id}>{game.name}</li>
+        ))}
+      </ul>
+    </>
+  );
 };
 
 export default GameGrid;

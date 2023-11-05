725be3555b2fc631f8ba0ebc7618029aaf891318
almondkiruthu <113090717+almondkiruthu@users.noreply.github.com>
2023-11-05 20:53:28 +0300

    Add skeleton logic

---
+++ src/components/game-card-skeleton.tsx
@@ -0,0 +1,5 @@
+const GameCardSkeleton = () => {
+  return <div className='animate-spin'>Loading!</div>;
+};
+
+export default GameCardSkeleton;
--- src/components/game-grid.tsx
+++ src/components/game-grid.tsx
@@ -1,12 +1,16 @@
 import useGames from '@/hooks/use-games.ts';
 import GameCard from './game-card';
+import GameCardSkeleton from './game-card-skeleton';
 
 const GameGrid = () => {
-  const { error, games } = useGames();
+  const { error, games, isLoading } = useGames();
+  const skeletons = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
   return (
     <>
       {error && <p>{error}</p>}
       <div className='grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-5'>
+        {isLoading &&
+          skeletons.map((skeleton) => <GameCardSkeleton key={skeleton} />)}
         {games.map((game) => (
           <GameCard key={game.id} game={game} />
         ))}

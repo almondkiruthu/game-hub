ca47f8907a6c2230cb59e4c6adfb1cadc7712981
almondkiruthu <113090717+almondkiruthu@users.noreply.github.com>
2023-11-05 23:55:32 +0300

    display genres

--- src/components/navbarComponents/sidebar-routes.tsx
+++ src/components/navbarComponents/sidebar-routes.tsx
@@ -1,14 +1,21 @@
 import useData from '@/hooks/use-genres';
+import getCroppedImageUrl from '@/services/image-url';
 
 const SideBarRoutes = () => {
   const { data } = useData();
   return (
     <div className='flex flex-col w-full'>
-      <ul>
+      <div className='mx-auto'>
         {data.map((genre) => (
-          <li key={genre.id}>{genre.name}</li>
+          <div key={genre.id} className='flex items-center gap-x-2 mt-3'>
+            <img
+              className='w-10 h-10 rounded-lg'
+              src={getCroppedImageUrl(genre.image_background)}
+            />
+            <p className='text-sm text-muted-foreground'>{genre.name}</p>
+          </div>
         ))}
-      </ul>
+      </div>
     </div>
   );
 };

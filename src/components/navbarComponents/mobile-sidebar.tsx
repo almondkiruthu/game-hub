4e07ebaf521edc3c8fad703ce2c41288cad6cbde
almondkiruthu <113090717+almondkiruthu@users.noreply.github.com>
2023-11-11 15:19:39 +0300

    Build: For Production

--- src/components/navbarComponents/mobile-sidebar.tsx
+++ src/components/navbarComponents/mobile-sidebar.tsx
@@ -1,15 +1,28 @@
 import { Menu } from 'lucide-react';
 import { Sheet, SheetContent, SheetTrigger } from '../ui/sheet';
 import Sidebar from './sidebar';
+import { Genre } from '@/hooks/use-genres';
+import { Platform } from '@/hooks/use-games';
+import { useState } from 'react';
 
+export interface GameQuery {
+  genre: Genre | null;
+  platform: Platform | null;
+  sortOrder: string;
+  searchText: string;
+}
 const MobileSidebar = () => {
+  const [gameQuery, setGameQuery] = useState<GameQuery>({} as GameQuery);
   return (
     <Sheet>
       <SheetTrigger className='md:hidden hover:opacity-75 transition'>
         <Menu />
       </SheetTrigger>
       <SheetContent side='left' className='p-1'>
-        <Sidebar />
+        <Sidebar
+          selectedGenre={gameQuery.genre}
+          onSelectGenre={(genre) => setGameQuery({ ...gameQuery, genre })}
+        />
       </SheetContent>
     </Sheet>
   );

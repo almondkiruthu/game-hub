609183580e998a00389b749753ef7efde6ff60a9
almondkiruthu <113090717+almondkiruthu@users.noreply.github.com>
2023-11-11 15:09:03 +0300

    Refactor game grid

--- src/components/game-grid.tsx
+++ src/components/game-grid.tsx
@@ -2,6 +2,7 @@ import useGames from '@/hooks/use-games.ts';
 import GameCard from './game-card';
 import GameCardSkeleton from './game-card-skeleton';
 import { GameQuery } from '@/App';
+import toast from 'react-hot-toast';
 
 interface GameGridProps {
   gameQuery: GameQuery;
@@ -10,17 +11,17 @@ interface GameGridProps {
 const GameGrid = ({ gameQuery }: GameGridProps) => {
   const { error, data, isLoading } = useGames(gameQuery);
   const skeletons = Array.from({ length: 20 }, (_, i) => i);
+  if (error) {
+    toast.error("Can't fetch games");
+  }
   return (
-    <>
-      {error && <p>{error}</p>}
-      <div className='grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4'>
-        {isLoading &&
-          skeletons.map((skeleton) => <GameCardSkeleton key={skeleton} />)}
-        {data.map((game) => (
-          <GameCard key={game.id} game={game} />
-        ))}
-      </div>
-    </>
+    <div className='grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4'>
+      {isLoading &&
+        skeletons.map((skeleton) => <GameCardSkeleton key={skeleton} />)}
+      {data.map((game) => (
+        <GameCard key={game.id} game={game} />
+      ))}
+    </div>
   );
 };
 

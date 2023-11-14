0a6ebecf64a96fcff3db18a487404754cced6936
almondkiruthu <113090717+almondkiruthu@users.noreply.github.com>
2023-11-14 13:06:04 +0300

    Implement infinite scroll

--- src/components/game-grid.tsx
+++ src/components/game-grid.tsx
@@ -1,55 +1,52 @@
+import { GameQuery } from '@/App';
 import useGames from '@/hooks/use-games.ts';
+import { Loader2 } from 'lucide-react';
+import React from 'react';
+import toast from 'react-hot-toast';
+import InfiniteScroll from 'react-infinite-scroll-component';
 import GameCard from './game-card';
 import GameCardSkeleton from './game-card-skeleton';
-import { GameQuery } from '@/App';
-import toast from 'react-hot-toast';
-import React from 'react';
-import { Button } from './ui/button';
-import { Loader2 } from 'lucide-react';
 
 interface GameGridProps {
   gameQuery: GameQuery;
 }
 
 const GameGrid = ({ gameQuery }: GameGridProps) => {
-  const {
-    error,
-    data,
-    isLoading,
-    isFetchingNextPage,
-    hasNextPage,
-    fetchNextPage,
-  } = useGames(gameQuery);
+  const { error, data, isLoading, hasNextPage, fetchNextPage } =
+    useGames(gameQuery);
+
   const skeletons = Array.from({ length: 20 }, (_, i) => i);
+
   if (error) {
     toast.error("Can't fetch games");
   }
+  const fetchedGamesCount =
+    data?.pages.reduce((acc, page) => acc + page.results.length, 0) || 0;
   return (
     <>
-      <div className='grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4'>
-        {isLoading &&
-          skeletons.map((skeleton) => <GameCardSkeleton key={skeleton} />)}
-        {data?.pages.map((page, index) => (
-          <React.Fragment key={index}>
-            {page.results.map((game) => (
-              <GameCard key={game.id} game={game} />
-            ))}
-          </React.Fragment>
-        ))}
-      </div>
-      <div className='pl-4'>
-        {hasNextPage && (
-          <Button variant='outline' onClick={() => fetchNextPage()}>
-            {!isFetchingNextPage && 'Load More'}
-            {isFetchingNextPage && (
-              <div className='flex items-center'>
-                <Loader2 className='mr-2 h-4 w-4 animate-spin' />
-                Please wait
-              </div>
-            )}
-          </Button>
-        )}
-      </div>
+      <InfiniteScroll
+        dataLength={fetchedGamesCount}
+        hasMore={!!hasNextPage}
+        next={() => fetchNextPage()}
+        loader={
+          <div className='flex items-center pl-4 gap-x-2 justify-center my-4'>
+            <Loader2 className='mr-2 h-4 w-4 animate-spin' />
+            Please wait
+          </div>
+        }
+      >
+        <div className='grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4'>
+          {isLoading &&
+            skeletons.map((skeleton) => <GameCardSkeleton key={skeleton} />)}
+          {data?.pages.map((page, index) => (
+            <React.Fragment key={index}>
+              {page.results.map((game) => (
+                <GameCard key={game.id} game={game} />
+              ))}
+            </React.Fragment>
+          ))}
+        </div>
+      </InfiniteScroll>
     </>
   );
 };
